96cda342e5aff407acbf1d5417160152b950b5c6
Amourlive <aleksei_mackevich@outlook.com>
2018-03-14 18:52:35 +0300

    add methods #can_write_to_column?, refactoring #move, fix generate_params and win_by method

--- connect_four.rb
+++ connect_four.rb
@@ -1,10 +1,11 @@
 class Game #:nodoc:
   def initialize
     @matrix = []
-    @step = 0
+    @step = 1
     @player_win = 0
     @switch = 0
     @switch_diagonal = 0
+    @arr = []
     # Sets the matrix size constraint
     @limitation_value_min = 3
     @limitation_value_max = 100
@@ -20,13 +21,36 @@ class Game #:nodoc:
 
     puts "Enter the number of connected chips to win (#{@limitation_value_min} <= number < = #{@matrix_width})"
     @chips_to_win = gets_valid_params @limitation_value_min, @matrix_width
-
     generate_params
   end
 
   def move
     loop do
+      if @step == @matrix_height * @matrix_width
+        puts 'Draw'
+        break
+      end
 
+      if (@step % 2).zero?
+        puts 'Second player makes a move'
+      else
+        puts 'First player makes a move'
+      end
+
+      number = gets_valid_params 1, @matrix_width
+      unless can_write_to_column?(number)
+        puts 'Your column is full, choose another one!'
+        redo
+      end
+      number += (@side_matrix_width - 1)
+      write_to_matrix(number)
+      puts @switch_diagonal
+      show_matrix
+      if current_player_win?
+        puts 'YOU WIN'
+        break
+      end
+      @step += 1
     end
   end
 
@@ -45,7 +69,14 @@ class Game #:nodoc:
     end
   end
 
-  def write_to_matrix
+  # returns true if you can add a chip to the column
+  def can_write_to_column?(number)
+    return false if @arr[number - 1] == @matrix_height
+    @arr[number - 1] += 1
+    true
+  end
+
+  def write_to_matrix(number)
     return @matrix[number] << 'x' unless (@step % 2).zero?
     @matrix[number] << 'o'
   end
@@ -54,21 +85,28 @@ class Game #:nodoc:
     # generate value for method include_chip?
     @value1 = 'x' * @chips_to_win
     @value2 = 'o' * @chips_to_win
+
     # generate value for enable method validation_vertical ...
     @switch = @chips_to_win * 2 - 1
     (0..@chips_to_win).each { |value| @switch_diagonal += value }
+
     # generate value for side matrix
     @side_matrix_width = @matrix_height - @chips_to_win
+    puts @side_matrix_width
     if @side_matrix_width < 0
       @side_matrix_width = 0
-    elsif @side_matrix_width > 0
+    else
       (0..@side_matrix_width - 1).each do |key|
-        (0..@matrix_height).each { |key2| @matrix[key][key2] << 'z' }
+        @matrix[key] = []
+        (0..@matrix_height).each { |key2| @matrix[key][key2] = 'z' }
       end
-      (@side_matrix_width + @matrix_width - 1..@matrix_width + 2 * @side_matrix_width - 1).each do |key|
-        (0..@matrix_height).each { |key2| @matrix[key][key2] << 'z' }
+      (@side_matrix_width..@side_matrix_width + @matrix_width - 1).each { |key| @matrix[key] = [] }
+      (@side_matrix_width + @matrix_width..@matrix_width + 2 * @side_matrix_width - 1).each do |key|
+        @matrix[key] = []
+        (0..@matrix_height).each { |key2| @matrix[key][key2] = 'z' }
       end
     end
+    (0..@matrix_width).each { |key| @arr[key] = 0 }
   end
 
   # checks if there is a necessary amount of chips inside the array
@@ -120,16 +158,16 @@ class Game #:nodoc:
   # validation work if the number of moves is enough to win
   # and the required number of chips can fit diagonally.
   def win_by_diagonal?
-    if @step > @switch_diagonal && @matrix_height >= @chips_to_win
+    if @step >= @switch_diagonal && @matrix_height >= @chips_to_win
       return true if win_by_diagonal_left? || win_by_diagonal_right?
     end
     false
   end
 
   def win_by_diagonal_left?
-    (0..@matrix_width + @side_matrix_width - @chips_to_win + 1).each do |index|
+    (0..@matrix_width + @side_matrix_width - @chips_to_win).each do |index|
       diagonal = []
-      (0..@matrix_height).each do |key|
+      (0..@matrix_height - 1).each do |key|
         key2 = index + key
         diagonal << fill_cell(@matrix[key2][key], 'z')
       end
@@ -139,9 +177,9 @@ class Game #:nodoc:
   end
 
   def win_by_diagonal_right?
-    (@side_matrix_width + @chips_to_win..@matrix_width + 2 * @side_matrix_width).to_a.reverse.each do |index|
+    (@side_matrix_width + @chips_to_win - 1..@matrix_width + 2 * @side_matrix_width - 1).to_a.reverse.each do |index|
       diagonal = []
-      (0..@matrix_height).each do |key|
+      (0..@matrix_height - 1).each do |key|
         key2 = index - key
         diagonal << fill_cell(@matrix[key2][key], 'z')
       end
@@ -157,9 +195,9 @@ class Game #:nodoc:
 
   # show matrix for user
   def show_matrix
-    (0..@matrix_height).to_a.reverse.each do |key|
+    (0..@matrix_height - 1).to_a.reverse.each do |key|
       print '|'
-      (@side_matrix_width..@matrix_width + @side_matrix_width).each do |key2|
+      (@side_matrix_width..@matrix_width + @side_matrix_width - 1).each do |key2|
         print fill_cell(@matrix[key2][key], ' '), '|'
       end
       print "\n"
@@ -169,3 +207,4 @@ end
 
 game = Game.new
 game.generate_game
+game.move
